b3579c7c8aebd67d45cc29391adef383973f2055
leo <leo.avril1908@gmail.com>
2024-02-08 21:59:13 +0100

    app

--- app.py
+++ app.py
@@ -8,31 +8,6 @@ app = Flask(__name__)
 items = []
 
 
-@app.route('/staging', methods=['POST'])
-def staging():
-    payload = request.json
-    ref = payload.get('ref', '')
-    response = ('', 204)
-    if ref == 'refs/heads/staging':
-        os.system("git pull origin staging")
-        os.system("pip3 install -r requirements.txt")
-        os.system("python -m unittest test-app.py")
-        os.system("python test-endtoend-app.py")
-        response = 'Test run successfuly'
-    return response
-
-
-@app.route('/deploy', methods=['POST'])
-def deploy():
-    payload = request.json
-    ref = payload.get('ref', '')
-    response = ('', 204)
-    if ref == 'refs/heads/main':
-        subprocess.call(['sh', './deploy.sh'])
-        response = 'App is running'
-    return response
-
-
 @app.route('/')
 def index():
     return render_template('index.html', items=items)
